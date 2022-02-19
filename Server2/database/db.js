e0b9f547d813ef681d0d095bdac2e765406d0677
Gabriel Mc Gann <gmcgann@proveedores.techint.com>
2022-02-18 23:04:33 -0300

    serv 2 anda

--- Server2/database/db.js
+++ Server2/database/db.js
@@ -28,9 +28,25 @@ db.Sequelize = Sequelize
 
 
 //syncronizar
-sequelize.sync({force:true})
+sequelize.sync({force:false})
 .then( ()=> {
     console.log("tablas creadas")
 })
 
 module.exports = db
+
+
+/* Host
+ec2-18-233-27-224.compute-1.amazonaws.com
+Database
+d7ksaibf33u9o4
+User
+yonqruomtantkm
+Port
+5432
+Password
+68587a81147f1051e99a08390811aa0dbf3a81f399d4cea18876a95e707e7f14
+URI
+postgres://yonqruomtantkm:68587a81147f1051e99a08390811aa0dbf3a81f399d4cea18876a95e707e7f14@ec2-18-233-27-224.compute-1.amazonaws.com:5432/d7ksaibf33u9o4
+Heroku CLI
+heroku pg:psql postgresql-amorphous-43867 --app mercaditoapi */
